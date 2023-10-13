8bf062bf70e8a695a4529e13422977bcde2ed2a7
JoseNeto5689 <jose.sicupiraneto@gmail.com>
2023-10-13 15:05:23 -0300

    API started

---
+++ src/main/java/com/osvaldo/adsdungeons/AdsdungeonsApplication.java
@@ -0,0 +1,13 @@
+package com.osvaldo.adsdungeons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdsdungeonsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdsdungeonsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/osvaldo/adsdungeons/AdsdungeonsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.osvaldo.adsdungeons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdsdungeonsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
