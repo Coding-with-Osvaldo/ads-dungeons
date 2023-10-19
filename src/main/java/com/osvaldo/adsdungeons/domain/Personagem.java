c7516764e784770b40b9800f401e5841f0635c07
JoseNeto5689 <jose.sicupiraneto@gmail.com>
2023-10-19 15:37:00 -0300

    Changing dependecies

--- src/main/java/com/osvaldo/adsdungeons/domain/Personagem.java
+++ src/main/java/com/osvaldo/adsdungeons/domain/Personagem.java
@@ -8,8 +8,7 @@ import java.util.List;
 import java.util.UUID;
 
 
-@Entity
-@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+@Entity @Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 public abstract class Personagem {
 
     @Id
